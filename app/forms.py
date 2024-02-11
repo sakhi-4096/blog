d115ed2b5d89d3a5b9c6b524791ca9c206f146fc
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2024-02-11 09:13:46 +0200

    Fix 'DataRequired' typo on class import.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DateRequired
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DateRequired()])
-    password = PasswordField('Password', validators=[DateRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = Booleanield('Remember Me')
     submit = SubmitField('Sign In')
