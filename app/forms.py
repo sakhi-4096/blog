142630b8162c3ae11a6e375cb8802ad794f9cc52
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2024-02-11 09:07:58 +0200

    Fix import typo of 'DateRequired' module.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DateRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DateRequired()])
